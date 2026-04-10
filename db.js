e7cf94c86acbe3c638ec3fd249188ca47004f5a7
rg7606 <rg7606@srmist.edu.in>
2026-04-10 14:05:18 +0530

    backend connected to railway database

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "mainline.proxy.rlwy.net",
   user: "root",
-  password: "", // XAMPP default is empty
-  database: "gym_booking",
+  password: "jaryQHBUoiLmasfusxyDNWPZjhBuQxtl",
+  database: "railway",
+  port: 31065,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err);
-    return;
+    console.error("❌ Database connection failed:", err.message);
+  } else {
+    console.log("✅ Connected to Railway MySQL Database");
   }
-  console.log("Connected to MySQL Database");
 });
 
 module.exports = db;
