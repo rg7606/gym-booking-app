12a009de490f959833c2fb4a208a5b153bdc1e33
rg7606 <rg7606@srmist.edu.in>
2026-04-10 14:10:38 +0530

    Added env variables support

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "mainline.proxy.rlwy.net",
-  user: "root",
-  password: "jaryQHBUoiLmasfusxyDNWPZjhBuQxtl",
-  database: "railway",
-  port: 31065,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
